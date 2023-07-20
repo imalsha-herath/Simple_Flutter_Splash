2719d05d2772786f01568f951f09f61e40c9017f
imalsha herath <imalshamalindih@gmail.com>
2023-07-20 19:56:07 +0530

    first commit

---
+++ lib/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class home extends StatelessWidget{
+
+@override
+Widget build(BuildContext context){
+  return Scaffold(
+
+    appBar: AppBar(
+
+      title: Text(
+        'Welcome To Home Page!',
+      ),
+      
+      
+    ),
+  
+     
+  );
+}
+}
+
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/home_page.dart';
+
+
+void main() {
+  runApp( MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+
+       debugShowCheckedModeBanner: false,
+      home: home(),
+
+      
+    );
+    
+  }
+}
